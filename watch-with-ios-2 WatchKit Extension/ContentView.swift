415176ddbf57c800c9b14d4ea817020544461714
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-05-14 05:50:46 -0500

    progress

--- watch-with-ios-2 WatchKit Extension/ContentView.swift
+++ watch-with-ios-2 WatchKit Extension/ContentView.swift
@@ -5,7 +5,7 @@ struct ContentView: View {
     let connectionProvider = ConnectionProvider.instance
 
     var body: some View {
-        Button("Send to Watch") {
+        Button("Send to Phone") {
             connectionProvider.sendValue(key: "text", value: "from watch")
         }
         .buttonStyle(.borderedProminent)
